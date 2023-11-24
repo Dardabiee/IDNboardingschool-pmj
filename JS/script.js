4d116745798f77ecb3f441bde279ba2d96d9b36c
Muhammad Darda Fathammubienaa <muhammad.darda@Huawae-MacBook-Pro.local>
2023-11-24 08:37:14 +0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+// scroll reveal
+ScrollReveal({
+    // reset: true ,
+    distance: '80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home,.heading,.navbar,.logo', { origin: 'top' });
+ScrollReveal().reveal('.about-container,.info-info p', { origin: 'top' });
+ScrollReveal().reveal('.owner,.about-box', { origin: 'bottom' });
+ScrollReveal().reveal('.contact', { origin: 'top' });
